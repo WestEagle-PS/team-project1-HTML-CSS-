c0c7f26cca2d456f578d6b68b1d70e3ed5440bb8
WestEagle-PS <anton_orlov@ukr.net>
2022-12-01 14:04:41 +0000

    Deploying to gh-pages from @ WestEagle-PS/team-project1-HTML-CSS-@b162b102732b7bb6952e2cedd9a9df356d1ad0d5 🚀

---
+++ index.44159a8a.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[download-modal-open]"),closeModalBtn:document.querySelector("[download-modal-close]"),modal:document.querySelector("[download-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.44159a8a.js.map
