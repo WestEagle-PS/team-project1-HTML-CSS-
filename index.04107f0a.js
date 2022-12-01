00a9a383d54174ddc69234083c62ef4897999699
WestEagle-PS <anton_orlov@ukr.net>
2022-12-01 17:25:45 +0000

    Deploying to gh-pages from @ WestEagle-PS/team-project1-HTML-CSS-@4cbe4b3c58c0bdbe21aea5ecfe849e216d6b3d59 🚀

---
+++ index.04107f0a.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[house-modal-open]"),closeModalBtn:document.querySelector("[house-modal-close]"),modal:document.querySelector("[house-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.04107f0a.js.map
